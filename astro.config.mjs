34ec8b5ed0971caf7744f7f800218851a725a183
wilgoz <willyghozali@gmail.com>
2025-07-12 20:40:56 +0700

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://hamantara.com",
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) => !page.includes("/thank-you"),
+    }),
+  ],
 });
